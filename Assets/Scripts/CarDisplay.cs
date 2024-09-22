05c94cbcb61bf4c4462d4153820bc0c11cf843b9
Polycarp Atalor <polycarpatalor@gmail.com>
2024-09-22 11:44:27 +0100

    Vehicle unlock system test

--- Assets/Scripts/CarDisplay.cs
+++ Assets/Scripts/CarDisplay.cs
@@ -15,31 +15,40 @@ public class CarDisplay : MonoBehaviour
     [SerializeField] private Image carSpeed;
     [SerializeField] private Image carAcceleration;
     [SerializeField] private Image carHandling;
-    
+
     [Header("3D Model")]
     [SerializeField] private GameObject carHolder;
 
+    [Header("UI Elements")]
     [SerializeField] private GameObject locked;
     [SerializeField] private GameObject unlockButton;
     [SerializeField] private GameObject play;
     [SerializeField] private GameObject paintUpgrade;
     [SerializeField] private GameObject tireUpgrade;
 
+    private Car currentCar;
+
     public void DisplayCar(Car _car)
     {
-         carName.text = _car.carName;
-         carDescription.text = _car.carDescription;
-         carPrice.text = _car.carPrice.ToString();
-         
-         carSpeed.fillAmount = (float)_car.speed / 100;
-         carHandling.fillAmount = (float)_car.handling / 100;
-         carAcceleration.fillAmount = (float)_car.acceleration / 100;
+        currentCar = _car;
 
-         if(carHolder.transform.childCount > 0)
+        // Update the car's name, description, and price
+        carName.text = _car.carName;
+        carDescription.text = _car.carDescription;
+        carPrice.text = _car.carPrice.ToString();
+
+        // Update the car's stats
+        carSpeed.fillAmount = (float)_car.speed / 100;
+        carHandling.fillAmount = (float)_car.handling / 100;
+        carAcceleration.fillAmount = (float)_car.acceleration / 100;
+
+        // Replace the current 3D car model if one exists
+        if (carHolder.transform.childCount > 0)
             Destroy(carHolder.transform.GetChild(0).gameObject);
 
-            Instantiate(_car.carModel, carHolder.transform.position, carHolder.transform.rotation, carHolder.transform);
-         
+        Instantiate(_car.carModel, carHolder.transform.position, carHolder.transform.rotation, carHolder.transform);
+
+        // Check if the car is unlocked based on the player's progress (using the car index)
         bool carUnlocked = PlayerPrefs.GetInt("currentScene", 0) >= _car.carIndex;
         locked.SetActive(!carUnlocked);
         play.SetActive(carUnlocked);
@@ -47,9 +56,45 @@ public class CarDisplay : MonoBehaviour
         tireUpgrade.SetActive(carUnlocked);
         unlockButton.SetActive(!carUnlocked);
 
-        // if(carUnlocked)
-        //   itemImage.color = Color.white;
-        //   else
-        //   itemImage.color = Color.gray;
+        // Set up unlock button to trigger purchase if car is locked
+        if (!carUnlocked)
+        {
+            unlockButton.GetComponent<Button>().onClick.RemoveAllListeners(); // Remove any previous listeners
+            unlockButton.GetComponent<Button>().onClick.AddListener(PurchaseCar);
+        }
+    }
+
+    // Attempt to purchase the car
+    private void PurchaseCar()
+    {
+        if (Currency.Instance != null)
+        {
+            int carPriceValue = currentCar.carPrice;
+
+            if (Currency.Instance.SpendMoney(carPriceValue))
+            {
+                Debug.Log("Car purchased successfully!");
+                // Unlock the car
+                PlayerPrefs.SetInt("currentScene", currentCar.carIndex); // Unlock the car in PlayerPrefs
+                PlayerPrefs.Save();
+
+                // Update UI after purchase
+                locked.SetActive(false);
+                play.SetActive(true);
+                paintUpgrade.SetActive(true);
+                tireUpgrade.SetActive(true);
+                unlockButton.SetActive(false);
+
+                Debug.Log($"New Balance: {Currency.Instance.GetBalance()} credits.");
+            }
+            else
+            {
+                Debug.Log("Not enough currency to purchase this car.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Currency system is not initialized.");
+        }
     }
 }
